d18d479a705ad69ff947cdbacfe9058faf101199
Bharath1910 <av.bharath@proton.me>
2023-11-11 22:42:02 +0530

    added new route to root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const prisma = require('./utils/prisma');
+const {execute: signup} = require('./routes/signup');
 
 const app = express();
+
+app.post('/signup', (req, res) => signup(req, res, prisma));
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
