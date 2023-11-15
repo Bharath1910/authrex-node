fd288d0bcfd7c0592e6367b64118299e9663e978
Bharath1910 <av.bharath@proton.me>
2023-11-15 19:34:04 +0530

    added test cases for token route

---
+++ __tests__/token.test.js
@@ -0,0 +1,33 @@
+const {generateApiKey, main} = require('../src/routes/token');
+const crypto = require('crypto');
+const {PrismaClient} = require('@prisma/client');
+const {StatusCodes} = require('http-status-codes');
+
+const prisma = new PrismaClient();
+
+jest.mock('crypto');
+jest.mock('@prisma/client');
+describe('API Key test cases', () => {
+  let req = {query: {}};
+  const res = {status: jest.fn().mockReturnThis(), send: jest.fn()};
+
+  afterEach(() => {
+    req = {query: {}};
+    jest.clearAllMocks();
+  });
+
+  it('should return 400 if type is wrong', async () => {
+    req.query.t = 'wrong';
+    await main(req, res, prisma);
+    expect(res.status).toHaveBeenCalledWith(StatusCodes.BAD_REQUEST);
+  });
+
+  it('should generate a random token', async () => {
+    crypto.randomBytes = jest.fn().mockReturnValue('random');
+    prisma.users = {update: jest.fn()};
+    await generateApiKey(req, res, prisma);
+    expect(crypto.randomBytes).toHaveBeenCalledTimes(1);
+    expect(prisma.users.update).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+  });
+});
