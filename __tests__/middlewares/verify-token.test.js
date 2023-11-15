eafe7932a92168483b3c358a410fd58593b93ca3
Bharath1910 <av.bharath@proton.me>
2023-11-15 13:42:50 +0530

    token verification middleware completed

---
+++ __tests__/middlewares/verify-token.test.js
@@ -0,0 +1,62 @@
+const {StatusCodes} = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+
+const {main: verifyToken} = require('../../src/middleware/verify-token');
+
+jest.mock('jsonwebtoken');
+
+describe('verifyToken middleware', () => {
+  let req; let res; let next;
+
+  beforeEach(() => {
+    req = {headers: {}};
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  test('should pass with a valid token', () => {
+    const token = 'valid_token';
+    req.headers['authorization'] = `Bearer ${token}`;
+    const decodedToken = {id: 'user_id'};
+
+    jwt.verify.mockImplementationOnce((token, secret, callback) => {
+      callback(null, decodedToken);
+    });
+
+    verifyToken(req, res, next);
+
+    expect(jwt.verify).toHaveBeenCalledWith(
+        token,
+        process.env.JWT_SECRET,
+        expect.any(Function),
+    );
+    expect(req.userId).toBe(decodedToken.id);
+    expect(next).toHaveBeenCalled();
+  });
+
+  test('should return UNAUTHORIZED with no token provided', () => {
+    verifyToken(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(StatusCodes.UNAUTHORIZED);
+    expect(res.send).toHaveBeenCalledWith({message: 'No token provided.'});
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  test('should return UNAUTHORIZED with invalid token', () => {
+    const token = 'invalid_token';
+    req.headers['authorization'] = `Bearer ${token}`;
+
+    jwt.verify.mockImplementationOnce((token, secret, callback) => {
+      callback(new Error('Invalid token'));
+    });
+
+    verifyToken(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(StatusCodes.UNAUTHORIZED);
+    expect(res.send).toHaveBeenCalledWith({message: 'Unauthorized.'});
+    expect(next).not.toHaveBeenCalled();
+  });
+});
--- src/middleware/verify-token.js
+++ src/middleware/verify-token.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const {StatusCodes} = require('http-status-codes');
+// eslint-disable-next-line no-unused-vars
+const express = require('express');
 
 /**
  * @param {jwt} jwt
@@ -7,25 +9,25 @@ const {StatusCodes} = require('http-status-codes');
  */
 function verifyToken(jwt) {
   /**
-   * @param {Request} req
-   * @param {Response} res
-   * @param {function} next
-   * @return {function}
+   * @param {express.Request} req
+   * @param {express.Response} res
+   * @param {express.NextFunction} next
    */
   return (req, res, next) => {
     const header = req.headers['authorization'];
     if (!header) {
-      return res
-          .status(StatusCodes.UNAUTHORIZED)
+      console.log(res.status().send());
+      res.status(StatusCodes.UNAUTHORIZED)
           .send({message: 'No token provided.'});
+      return;
     }
 
     const token = header.split(' ')[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
-        return res
-            .status(StatusCodes.UNAUTHORIZED)
-            .send({message: 'Unauthorized!'});
+        res.status(StatusCodes.UNAUTHORIZED)
+            .send({message: 'Unauthorized.'});
+        return;
       }
       req.userId = decoded.id;
       next();
