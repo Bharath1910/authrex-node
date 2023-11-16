ca34dfa08514ecb35776899249c53e086a03ed1a
Bharath1910 <av.bharath@proton.me>
2023-11-16 09:36:42 +0530

    added login options

--- src/routes/user.js
+++ src/routes/user.js
@@ -24,6 +24,50 @@ async function updateRedirect(req, res, prisma) {
 }
 
 /**
+ * @param {string} options
+ * @return {Array<string>}
+ * @throws {Error}
+*/
+function parseOptions(options) {
+  const parsedOptions = options.split(',');
+  const validOptions = ['username', 'pgp'];
+
+  parsedOptions.filter((option) => validOptions.includes(option));
+  if (parsedOptions.length === 0) {
+    throw new Error('Invalid options');
+  }
+
+  return parsedOptions;
+}
+
+/**
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {prisma} prisma
+*/
+async function updateOptions(req, res, prisma) {
+  console.log(req.query.options);
+  if (!req.query.options) {
+    return;
+  }
+
+  try {
+    parseOptions(req.query.options);
+  } catch (err) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: err.message,
+    });
+  }
+
+  await prisma.users.update({
+    where: {id: req.userId},
+    data: {options: req.query.options},
+  });
+
+  return;
+}
+
+/**
  * @param {express.Request} req
  * @param {express.Response} res
  * @param {prisma.PrismaClient} prisma
