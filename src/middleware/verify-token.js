1a3bcf5edaa73bc9a0211a0f6835fcd4224f7983
Bharath1910 <av.bharath@proton.me>
2023-11-18 23:49:23 +0530

    base key auth done

--- src/middleware/verify-token.js
+++ src/middleware/verify-token.js
@@ -1,19 +1,22 @@
 const jwt = require('jsonwebtoken');
 const {StatusCodes} = require('http-status-codes');
 // eslint-disable-next-line no-unused-vars
+const prisma = require('../utils/prisma');
+// eslint-disable-next-line no-unused-vars
 const express = require('express');
 
 /**
- * @param {jwt} jwt
+ * @param {prisma} prisma
+ * @param {Boolean} verifyKey
  * @return {function}
  */
-function verifyToken(jwt) {
+function verifyToken(prisma, verifyKey) {
   /**
    * @param {express.Request} req
    * @param {express.Response} res
    * @param {express.NextFunction} next
    */
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const header = req.headers['authorization'];
     if (!header) {
       console.log(res.status().send());
@@ -23,6 +26,22 @@ function verifyToken(jwt) {
     }
 
     const token = header.split(' ')[1];
+    if (verifyKey) {
+      user = await prisma.users.findUnique({
+        where: {
+          id: req.userId,
+        },
+        select: {
+          apiKey: true,
+        },
+      });
+
+      if (user.apiKey === token) {
+        next();
+        return;
+      }
+    }
+
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         res.status(StatusCodes.UNAUTHORIZED)
@@ -35,4 +54,4 @@ function verifyToken(jwt) {
   };
 }
 
-module.exports = {main: verifyToken(jwt), test: verifyToken};
+module.exports = {main: verifyToken};
